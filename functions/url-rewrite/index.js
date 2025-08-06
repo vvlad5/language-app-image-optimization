c90e4d7de786b5133ff4d497b2873c88b7736203
vvlad5 <galko3.v@gmail.com>
2025-08-06 16:10:56 +0300

    specify default safe image width value & refactoring

--- functions/url-rewrite/index.js
+++ functions/url-rewrite/index.js
@@ -2,59 +2,60 @@
 // SPDX-License-Identifier: MIT-0
 
 function handler(event) {
-    var request = event.request;
-    var originalImagePath = request.uri;
-    //  validate, process and normalize the requested operations in query parameters
-    var normalizedOperations = {};
-    if (request.querystring) {
-        Object.keys(request.querystring).forEach(operation => {
-            switch (operation.toLowerCase()) {
-                case 'format': 
-                    var SUPPORTED_FORMATS = ['auto', 'jpeg', 'webp', 'avif'];
-                    if (request.querystring[operation]['value'] && SUPPORTED_FORMATS.includes(request.querystring[operation]['value'].toLowerCase())) {
-                        var format = request.querystring[operation]['value'].toLowerCase(); // normalize to lowercase
+    const request = event.request;
+    const originalImagePath = request.uri;
 
-                        if (format === 'auto') {
-                            if (request.headers['accept']) {
-                                if (request.headers['accept'].value.includes("avif")) {
-                                    format = 'avif';
-                                } else if (request.headers['accept'].value.includes("webp")) {
-                                    format = 'webp';
-                                }
+    //  validate, process and normalize the requested operations in query parameters
+    const normalizedOperations = {};
+
+    Object.keys(request.querystring).forEach(operation => {
+        switch (operation.toLowerCase()) {
+            case 'format':
+                const SUPPORTED_FORMATS = ['auto', 'jpeg', 'webp', 'avif'];
+
+                if (request.querystring[operation]['value'] && SUPPORTED_FORMATS.includes(request.querystring[operation]['value'].toLowerCase())) {
+                    let format = request.querystring[operation]['value'].toLowerCase(); // normalize to lowercase
+
+                    if (format === 'auto') {
+                        if (request.headers['accept']) {
+                            if (request.headers['accept'].value.includes("avif")) {
+                                format = 'avif';
+                            } else if (request.headers['accept'].value.includes("webp")) {
+                                format = 'webp';
                             }
                         }
+                    }
 
-                        normalizedOperations['format'] =
-                          request.headers['accept'] && request.headers['accept'].value.includes(format)
-                            ? format : 'jpeg';
-                    }
-                    break;
-                case 'width':
-                    var SUPPORTED_WIDTH = [160, 196, 256, 384, 428, 640, 750, 828, 1080, 1200, 1920];
-                    if (request.querystring[operation]['value'] && SUPPORTED_WIDTH.includes(parseInt(request.querystring[operation]['value']))) {
-                        normalizedOperations['width'] = request.querystring[operation]['value'];
-                    }
-                    break;
-                default: break;
-            }
-        });
-        //rewrite the path to normalized version if valid operations are found
-        if (Object.keys(normalizedOperations).length > 0) {
-            // put them in order
-            var normalizedOperationsArray = [];
-            if (normalizedOperations.format) normalizedOperationsArray.push('format='+normalizedOperations.format);
-            if (normalizedOperations.width) normalizedOperationsArray.push('width='+normalizedOperations.width);
-            request.uri = originalImagePath + '/' + normalizedOperationsArray.join(',');     
-        } else {
-            // If no valid operation is found, flag the request with /original path suffix
-            request.uri = originalImagePath + '/original';     
+                    normalizedOperations['format'] =
+                      request.headers['accept'] && request.headers['accept'].value.includes(format)
+                        ? format : 'jpeg';
+                }
+
+                break;
+            case 'width':
+                const SUPPORTED_WIDTH = [160, 196, 256, 384, 428, 640, 750, 828, 1080, 1200, 1920];
+
+                if (request.querystring[operation]['value'] && SUPPORTED_WIDTH.includes(parseInt(request.querystring[operation]['value']))) {
+                    normalizedOperations['width'] = request.querystring[operation]['value'];
+                }
+
+                break;
+            default:
+                break;
         }
+    });
 
-    } else {
-        // If no query strings are found, flag the request with /original path suffix
-        request.uri = originalImagePath + '/original';
-    }
+    //rewrite the path to normalized version, put them in order
+    const normalizedOperationsArray = [];
+
+    if (normalizedOperations.format) normalizedOperationsArray.push('format=' + normalizedOperations.format);
+    if (normalizedOperations.width) normalizedOperationsArray.push('width=' + normalizedOperations.width);
+    else normalizedOperationsArray.push('width=' + 10);
+
+    request.uri = originalImagePath + '/' + normalizedOperationsArray.join(',');
+
     // remove query strings
     request['querystring'] = {};
+
     return request;
 }
